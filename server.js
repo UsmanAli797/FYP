a5b046af9ff4fa0b86492008a19c4f74b16479c7
UsmanAli797 <52331287+UsmanAli797@users.noreply.github.com>
2022-06-25 16:15:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 var createError = require('http-errors');
-
+const fileUpload = require('express-fileupload');
 
 
 var cookieParser = require('cookie-parser');
-var expressSession = require('express-session');
+var sessions = require('express-session');
 var bodyParser     =   require("body-parser");
 
 var indexRouter = require('./routes/citizen');
@@ -17,21 +17,29 @@ var courtRouter = require('./routes/court');
 
 
 const app = express();
+app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false 
+}));
+
+
 app.use('/citizen', indexRouter);
 app.use('/police', policeRouter);
 app.use('/forensic', forensicRouter);
 app.use('/court', courtRouter);
-app.use(expressSession({secret: 'max' , saveUninitialized: false , resave: false}));
 app.use('/citizen', express.static('citizen/citizen'));
 app.use('/police', express.static('police/police'));
 app.use('/forensic', express.static('forensic/forensic'));
 app.use('/court', express.static('court/court'));
-
 
 
 
